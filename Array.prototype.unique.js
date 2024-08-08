7eb27226e9ad86a1ef71f27ea573c69c6a55e68d
Squid2112 <squid@thixo.com>
2024-08-08 15:45:56 -0500

    added Array unique prototype

---
+++ Array.prototype.unique.js
@@ -0,0 +1,9 @@
+Array.prototype.unique= function () {
+	return this.reduce(function(previous, current, index, array) {
+		previous[current.toString()+typeof(current)]=current;
+		return array.length-1 == index ? Object.keys(previous).reduce(function(prev,cur) {
+			prev.push(previous[cur]);
+			return prev;
+		},[]) : previous;
+	}, {});
+};
